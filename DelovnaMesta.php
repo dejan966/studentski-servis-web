48e585a2c9ddf825dcab5bcca7e8d0b1b141c9fb
dejan966 <59951914+dejan966@users.noreply.github.com>
2021-05-10 19:10:37 +0200

    test

--- DelovnaMesta.php
+++ DelovnaMesta.php
@@ -1,9 +1,25 @@
 <?php
 
 class DelovnaMesta {
-    public $ime;
+    public $Naziv;
+    public $Opis;
+    public $Placa;
+    public $Trajanje;
+    public $Delovnik;
+    public $Sifra;
+    public $Kraj;
+    public $Podjetje;
+    public $FilePath;
     
-    public function _construct($_ime){
-        $ime = $_ime;
+    public function DelovnaMesta($_naziv, $_opis, $_placa, $_trajanje, $_delovnik, $_sifra, $_prosto, $_kraj, $_podjetje, $_filepath){
+        $this -> Naziv = $_naziv;
+        $this -> Opis = $_opis;
+        $this -> Placa = $_placa;
+        $this -> Trajanje = $_trajanje;
+        $this -> Delovnik = $_delovnik;
+        $this -> Sifra = $_placa;
+        $this -> Kraj = $_kraj;
+        $this -> Podjetje = $_podjetje;
+        $this -> FilePath = $_filepath;
     }
 }
